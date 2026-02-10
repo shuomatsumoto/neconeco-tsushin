250d5ac35762e0e9ae96b280f5adc28b543b6888
Shuō Matsumoto <121488477+shuomatsumoto@users.noreply.github.com>
2026-02-10 12:03:11 +0900

    Set base only for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: "https://shuomatsumoto.github.io",
-  base: "/kamonegi-tsushin",
+  base: isProd ? "/kamonegi-tsushin" : "/",
 });
